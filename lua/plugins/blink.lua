46d88b1531b0fe1bf4dbb194670e0b95d53c1190
ph1losof <my.tentacles.are.in.you@gmail.com>
2025-10-24 21:22:45 +0200

    use codeium for ai completions

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,18 @@ return {
     'saghen/blink.cmp',
     lazy = false,
     dependencies = {
-      'fang2hou/blink-copilot',
+      {
+        'Exafunction/codeium.nvim',
+        cmd = 'Codeium',
+        event = 'InsertEnter',
+        build = ':Codeium Auth',
+        opts = {
+          enable_cmp_source = false,
+          virtual_text = {
+            enabled = false,
+          },
+        },
+      },
       {
         'L3MON4D3/LuaSnip',
         build = 'make install_jsregexp',
@@ -35,7 +46,7 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
         kind_icons = {
-          Copilot = '',
+          Codeium = '',
 
           Text = '󰉿',
           Method = '󰊕',
@@ -81,24 +92,24 @@ return {
           snippets = { score_offset = 101, max_items = 3, name = 'snippets', module = 'blink.cmp.sources.snippets' },
           lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
           lsp = { score_offset = 99, name = 'lsp', module = 'blink.cmp.sources.lsp' },
-          copilot = {
-            name = 'copilot',
-            module = 'blink-copilot',
+          codeium = {
+            name = 'Codeium',
+            module = 'codeium.blink',
             score_offset = 98,
-            async = true,
             transform_items = function(_, items)
               local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
               local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = 'Copilot'
+              CompletionItemKind[kind_idx] = 'Codeium'
               for _, item in ipairs(items) do
                 item.kind = kind_idx
               end
               return items
             end,
+            async = true,
           },
           dadbod = { name = 'dadbod', module = 'vim_dadbod_completion.blink' },
         },
-        default = { 'ecolog', 'copilot', 'snippets', 'lazydev', 'lsp', 'path', 'buffer' },
+        default = { 'ecolog', 'codeium', 'snippets', 'lazydev', 'lsp', 'path', 'buffer' },
         per_filetype = {
           codecompanion = { 'codecompanion' },
           sql = { 'snippets', 'dadbod', 'buffer' },

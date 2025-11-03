d477277e3182220641fee3f12d5162c3e451fbd5
ph1losof <my.tentacles.are.in.you@gmail.com>
2025-11-03 06:48:30 +0100

    update to newer standard

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -103,7 +103,7 @@ return {
             ['textDocument/publishDiagnostics'] = function(err, res, ...)
               local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ':t')
               if string.match(file_name, '^%.env') == nil then
-                return on_publish_diagnostics(err, res, ...)
+                return vim.lsp.diagnostic.on_publish_diagnostics(err, res, ...)
               end
             end,
           },
@@ -233,8 +233,10 @@ return {
 
       require('mason-lspconfig').setup {
         ensure_installed = {},
-        automatic_enable = true,
       }
+
+      local server_names = vim.tbl_keys(servers)
+      vim.lsp.enable(server_names)
     end,
   },
 }

08990f7a138de0e3e863c77dcf348ebcb1097c8b
ph1losof <my.tentacles.are.in.you@gmail.com>
2026-04-19 11:35:54 +0200

    feat(plugins): add nvumi command runner plugin
    Replace the removed cellmode.nvim lock entry with nvumi and add plugin config
    so the editor has an integrated command runner with predefined keybindings and
    inline output formatting.

---
+++ lua/plugins/nvumi.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'josephburgess/nvumi',
+    opts = {
+      virtual_text = 'newline',
+      prefix = ' = ',
+      date_format = 'iso',
+      width = 0.8,
+      height = 0.4,
+      keys = {
+        run = '<CR>',
+        reset = 'R',
+        yank = '<leader>y',
+        yank_all = '<leader>Y',
+      },
+      custom_conversions = {},
+      custom_functions = {},
+    },
+  },
+}
